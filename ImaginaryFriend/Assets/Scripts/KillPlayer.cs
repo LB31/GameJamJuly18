f8e18a3135614352973832e153a1ab3a006ae7f4
Zuramm <opheder@gmail.com>
2018-07-14 20:19:15 +0200

    Something

--- ImaginaryFriend/Assets/Scripts/KillPlayer.cs
+++ ImaginaryFriend/Assets/Scripts/KillPlayer.cs
@@ -6,7 +6,7 @@ public class KillPlayer : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+        SceneController.instance.ChangeToDeathScene();
     }
 
     // Use this for initialization
