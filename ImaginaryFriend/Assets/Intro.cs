c42549f2f8fcac09f883a4baddf9c334c0c38d43
julian <julianpieper@gmx.de>
2018-07-14 20:41:24 +0200

    introdone

--- ImaginaryFriend/Assets/Intro.cs
+++ ImaginaryFriend/Assets/Intro.cs
@@ -23,24 +23,31 @@ public class Intro : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Bedroom");
+
+
+        }
+    }
 
     public void NextIntroImage()
     {
 
-     
+     print("I got here");
         introImages[introCount].SetActive(false);
         introCount ++;
         if(introCount < introImages.Length)
         {
             introImages[introCount].SetActive(true);
+            
         }
         else
         {
             SceneManager.LoadScene("Bedroom");
         }
-
+        print(introCount);
+        
     }
 
 }
