dcacecddf71173ce30b329a9b0a3666d00e7da68
Leonid <leonidb93@gmail.com>
2018-07-14 19:43:51 +0200

    add nanny

---
+++ ImaginaryFriend/Assets/Scripts/NannyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NannyController : MonoBehaviour {
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<ChildIdentifier>().gameObject;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetDir = player.transform.position - transform.position;
+        float angle = Vector3.Angle(targetDir, transform.forward);
+
+        if (angle < 5.0f)
+            print("close");
+    }
+	}
+}
