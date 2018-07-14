41ed9d9c273d041061231becd6cc7c921d2ce1fd
Leonid <leonidb93@gmail.com>
2018-07-14 20:15:46 +0200

    nanny

--- ImaginaryFriend/Assets/Scripts/NannyController.cs
+++ ImaginaryFriend/Assets/Scripts/NannyController.cs
@@ -13,11 +13,14 @@ public class NannyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 targetDir = player.transform.position - transform.position;
-        float angle = Vector3.Angle(targetDir, transform.forward);
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
 
-        if (angle < 5.0f)
-            print("close");
+        Vector3 targetDir = player.transform.position - transform.position;
+        float angle = Vector3.Angle(targetDir, transform.forward);
+       
+        if (angle < 12.0f)
+            Debug.Log("close");
     }
-	}
+	
 }
